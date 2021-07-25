6f100d0b6a278dc4c0bd0130ecaba26b57a11ed2
dkwolfventures <64211151+dkwolfventures@users.noreply.github.com>
2021-07-25 11:09:01 -0500

    Initial Commit

---
+++ HuliPizza/HuliPizzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HuliPizzaApp.swift
+//  HuliPizza
+//
+//  Created by Coding on 7/25/21.
+//
+
+import SwiftUI
+
+@main
+struct HuliPizzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
